04a097eebd2508a4b73eaeec601a9da435fce54f
jackmannino <jack@nvisiumsecurity.com>
2013-05-31 17:34:30 -0400

    -Initial commit of the FourGoats PhoneGap app. Not much going on yet.

---
+++ OWASP_GoatDroid_FourGoats_PhoneGap/gen/org/owasp/goatdroid/fourgoats/phonegap/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.owasp.goatdroid.fourgoats.phonegap;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
