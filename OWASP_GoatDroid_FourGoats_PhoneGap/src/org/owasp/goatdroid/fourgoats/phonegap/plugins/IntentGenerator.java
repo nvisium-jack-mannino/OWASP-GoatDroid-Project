546f4b4240115785549ca536b4415e69bbf9c02f
jackmannino <jack@nvisiumsecurity.com>
2013-07-09 14:32:40 -0400

    -Added Intent Builder code to build an intent object from a map passed in via JS.

--- OWASP_GoatDroid_FourGoats_PhoneGap/src/org/owasp/goatdroid/fourgoats/phonegap/plugins/IntentGenerator.java
+++ OWASP_GoatDroid_FourGoats_PhoneGap/src/org/owasp/goatdroid/fourgoats/phonegap/plugins/IntentGenerator.java
@@ -1,9 +1,14 @@
 package org.owasp.goatdroid.fourgoats.phonegap.plugins;
 
+import java.util.HashMap;
 import org.apache.cordova.api.CallbackContext;
 import org.apache.cordova.api.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
 
 public class IntentGenerator extends CordovaPlugin {
 
@@ -18,5 +23,50 @@ public class IntentGenerator extends CordovaPlugin {
 			return true;
 		}
 		return false;
+	}
+
+	public Intent buildIntent(HashMap<String, Object> intentMap) {
+
+		Intent intent = new Intent();
+
+		if (intentMap.get("action") != null) {
+
+			intent.setAction((String) intentMap.get("action"));
+		}
+
+		if (intentMap.get("data") != null) {
+
+			intent.setData((Uri) intentMap.get("data"));
+		}
+
+		if (intentMap.get("category") != null) {
+
+			intent.addCategory((String) intentMap.get("category"));
+		}
+
+		if (intentMap.get("type") != null) {
+
+			intent.setType((String) intentMap.get("type"));
+
+		}
+
+		if (intentMap.get("component") != null) {
+
+			intent.setComponent((ComponentName) intentMap.get("component"));
+
+		}
+
+		if (intentMap.get("extras") != null) {
+
+			intent.putExtras((Bundle) intentMap.get("extras"));
+
+		}
+
+		if (intentMap.get("flags") != null) {
+
+			intent.setFlags((Integer) intentMap.get("flags"));
+		}
+
+		return intent;
 	}
 }
