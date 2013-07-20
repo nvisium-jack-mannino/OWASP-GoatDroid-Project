2379db49991432159c14572506046f80a85b2a21
jackmannino <jack.mannino@owasp.org>
2013-07-20 00:02:56 -0400

    -Progress to make index.jsp work properly when navigating to it from other pages..had to add an input parameter that's parsed to render the appropriate content.

--- OWASP GoatDroid- GUI/webapps/goatdroid/index.jsp
+++ OWASP GoatDroid- GUI/webapps/goatdroid/index.jsp
@@ -5,7 +5,7 @@
 <jsp:include page="application.html" />
 <script>
 	$(document).ready(function() {
-		initializeContext();
+		initializeContext($.url().param('foo'));
 	});
 </script>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
--- OWASP GoatDroid- GUI/webapps/goatdroid/js/goatdroid.js
+++ OWASP GoatDroid- GUI/webapps/goatdroid/js/goatdroid.js
@@ -1,19 +1,32 @@
 var DEFAULT_APP = "fourgoats-native";
 
-function initializeContext() {
+function initializeContext(app) {
 	if (getCookie() != 'null') {
+		console.log("in first " + app);
 		loadSidebar(DEFAULT_APP);
 		loadLesson(DEFAULT_APP);
 	} else {
+		console.log("in last " + app);
 		loadSidebar(getCookie());
 		loadLesson(getCookie());
 	}
 }
 
 function loadNewContent(app) {
-	loadLesson(app);
-	loadSidebar(app);
-	setCookie(app);
+	/*
+	 * If we aren't at index.jsp, we redirect to it and pass in the app as a
+	 * parameter. This method will get called again once you get onto thepage,
+	 * but it should hit the "else" where the app's content is properly
+	 * initialized
+	 * 
+	 */
+	if (window.location.pathname != "index.jsp") {
+		window.location.href = "index.jsp?app=" + app;
+	} else {
+		loadLesson(app);
+		loadSidebar(app);
+		setCookie(app);
+	}
 }
 
 function loadLesson(app) {
