9af24b7f8c1afdaf12b66f06654b8bcd9ac6729a
jackmannino <jack.mannino@owasp.org>
2013-07-28 02:28:06 -0400

    -Added Base64 decoding and validity checks for Authorization header username/token strings. -Implemented a decent amount of the authentication filter.

--- OWASP GoatDroid- GUI/src/org/owasp/goatdroid/webservice/filters/AuthenticationFilter.java
+++ OWASP GoatDroid- GUI/src/org/owasp/goatdroid/webservice/filters/AuthenticationFilter.java
@@ -1,25 +1,70 @@
 package org.owasp.goatdroid.webservice.filters;
 
 import java.io.IOException;
-import java.util.Enumeration;
-
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
-
-import org.eclipse.jetty.deploy.providers.WebAppProvider.Filter;
+import org.owasp.goatdroid.webservice.fourgoats.services.FGLoginServiceImpl;
+import org.springframework.security.crypto.codec.Base64;
 import org.springframework.web.filter.GenericFilterBean;
 
 public class AuthenticationFilter extends GenericFilterBean {
 
 	@Override
-	public void doFilter(ServletRequest arg0, ServletResponse arg1,
-			FilterChain arg2) throws IOException, ServletException {
-		final HttpServletRequest request = (HttpServletRequest) arg0;
-		String authorizationString = request.getHeader("authorization");
-		authorizationString.length();
-	}
+	public void doFilter(ServletRequest servletRequest,
+			ServletResponse servletResponse, FilterChain filterChain)
+			throws IOException, ServletException {
+		final HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+		/*
+		 * First we check to ensure that the string is real Base64 and not
+		 * garbage
+		 */
+		if (Base64.isBase64(httpServletRequest.getHeader("authorization")
+				.getBytes())) {
+			String authorizationString = new String(
+					Base64.decode(httpServletRequest.getHeader("authorization")
+							.getBytes()));
+			/*
+			 * We split the username and password on the first colon. Any colons
+			 * encountered afterwards are considered part of the password.
+			 */
+			String userName = "";
+			String token = "";
+			boolean isColonFound = false;
+			for (char ch : authorizationString.toCharArray()) {
+				if (ch == ':')
+					isColonFound = true;
+				else if (!isColonFound)
+					userName += ch;
+				else
+					token += ch;
+			}
 
+			/*
+			 * Check to make sure we had a username and a token, neither can be
+			 * blank
+			 */
+
+			/*
+			 * Now, we check to ensure the authentication token is valid for the
+			 * user account
+			 */
+			FGLoginServiceImpl loginService = new FGLoginServiceImpl();
+			loginService.validateAuthToken(userName, token);
+			/*
+			 * If valid, pass on the filter chain
+			 */
+
+			/*
+			 * Else, return 401 unauthorized
+			 */
+
+		} else {
+			/*
+			 * return 401 unauthorized
+			 */
+		}
+	}
 }
