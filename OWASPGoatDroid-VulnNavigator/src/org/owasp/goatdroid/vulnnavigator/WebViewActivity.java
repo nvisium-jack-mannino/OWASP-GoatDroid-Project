7c122efeeb51c4287e87e45c5d6a77269d94c054
jackmannino <jack@nvisium.com>
2013-11-21 18:36:57 -0500

    -Added layouts for a bunch of new activities.

--- OWASPGoatDroid-VulnNavigator/src/org/owasp/goatdroid/vulnnavigator/WebViewActivity.java
+++ OWASPGoatDroid-VulnNavigator/src/org/owasp/goatdroid/vulnnavigator/WebViewActivity.java
@@ -1,7 +1,21 @@
 package org.owasp.goatdroid.vulnnavigator;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
 
 public class WebViewActivity extends Activity {
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_webview);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 }
